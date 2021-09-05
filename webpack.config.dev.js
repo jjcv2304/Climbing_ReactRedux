ecc96ca5d5020907289bb4c9e4d12238f56e87a1
Juan Jose Carmona Vilas <juan.vilas@tsunamisolutions.com>
2021-09-05 11:52:13 -0700

    app url

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -24,7 +24,8 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+      // "process.env.API_URL": JSON.stringify("http://localhost:3001"),
+      "process.env.API_URL": JSON.stringify("http://127.0.0.1:3001"),
     }),
     new HtmlWebpackPlugin({
       template: "src/index.html",
